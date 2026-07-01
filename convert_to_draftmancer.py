6740900b68c1769708e0afb659757b0570e6d7f8
apokef1sh <apokefish@gmail.com>
2026-07-01 16:02:04 -0700

    v0.1.7

--- convert_to_draftmancer.py
+++ convert_to_draftmancer.py
@@ -4,11 +4,12 @@ class Card:
         self.name = ""
         self.manacost = ""
         self.type = ""
+        self.subtypes = ""
         self.oracle_text = ""
         self.pt = ""
 
     def __str__(self):
-        return f"name: {card.name}, manacost: {card.manacost}, type: {card.type}, oracle text: {card.oracle_text}"
+        return f"name: {card.name}, manacost: {card.manacost}, type: {card.type}, subtypes: {card.subtypes}, oracle text: {card.oracle_text}"
 
 
 filepath = input(">").replace("\"", "")
@@ -21,7 +22,12 @@ for new_card in filetext.split("<card>")[1:]:
     card.name = new_card.split("<name>")[1].split("</name>")[0].replace("&apos;", "")
     if "<manacost>" in new_card:
         card.manacost = new_card.split("<manacost>")[1].split("</manacost>")[0]
-    card.type = new_card.split("<type>")[1].split("</type>")[0].replace(" //", "")
+    types = new_card.split("<type>")[1].split("</type>")[0].replace(" //", "").split()
+    if ("-" in types):
+        card.type = " ".join(types[:(types.index("-"))])
+        card.subtypes = " ".join(types[(types.index("-")+1):])
+    else:
+        card.type = " ".join(types)
     card.oracle_text = new_card.split("<text>")[1].split("</text>")[0]
     if "<pt>" in new_card:
         card.pt = new_card.split("<pt>")[1].split("</pt>")[0]
@@ -51,12 +57,20 @@ for card in cards:
     else:
         layout = ""
     oracletext = card.oracle_text.replace("\n", "\\n")
+    subtype_text = ""
+    if card.subtypes != "":
+        subtype_text = "\n        \"subtypes\": ["
+        for subtype in card.subtypes.split():
+            subtype_text = subtype_text + f"\n             \"{subtype}\""
+            if subtype != card.subtypes.split()[len(card.subtypes.split())-1]:
+                subtype_text = subtype_text + ","
+        subtype_text = subtype_text + "\n        ],"
     fulltext = fulltext+f"""
     {{
         "id": "{uuid.uuid4()}",
         "name": "{card.name}",
         "mana_cost": "{card.manacost}",
-        "type": "{card.type}",
+        "type": "{card.type}",{subtype_text}
         "image": "https://raw.githubusercontent.com/apokef1sh/YIPEEE_cube/main/Images/{card.name.replace(" ", "%20").replace(",", "")}.png",
         "colors": [
             "{colortext}"
@@ -66,13 +80,12 @@ for card in cards:
     }}"""
     if not cards[len(cards)-1] == card:
         fulltext = fulltext + ","
-    print(fulltext)
 fulltext = fulltext + """\n]
 [MainSlot]\n"""
 for card in cards:
     if "Token" not in card.type:
         fulltext = fulltext +f"4 {card.name}\n"
-print(fulltext)
+# print(fulltext)
 with open("draftmanceroutput.txt", "w", encoding="utf-8") as draftmancerfile:
     draftmancerfile.write(fulltext)
     pass
